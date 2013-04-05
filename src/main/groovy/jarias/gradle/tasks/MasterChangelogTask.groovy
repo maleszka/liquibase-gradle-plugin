c73c10fb13e9ff2944fc26dfe07913dc1c25ae6d
Julio Arias <jarias01@gmail.com>
2013-04-05 17:58:57 +0200

    Add MasterChangelog empty task

---
+++ src/main/groovy/jarias/gradle/tasks/MasterChangelogTask.groovy
@@ -0,0 +1,10 @@
+package jarias.gradle.tasks
+
+import org.gradle.api.DefaultTask
+
+/**
+ * @author jarias
+ * @since 4/5/13 5:52 PM 
+ */
+class MasterChangelogTask extends DefaultTask {
+}
---
+++ src/test/groovy/jarias/gradle/tasks/MasterChangelogTaskTest.groovy
@@ -0,0 +1,8 @@
+package jarias.gradle.tasks
+
+/**
+ * @author jarias
+ * @since 4/5/13 5:58 PM 
+ */
+class MasterChangelogTaskTest {
+}
