c536da0f43db066b049e6983a19c15c8fdee89b2
Julio Arias <jarias01@gmail.com>
2013-04-05 19:18:36 +0200

    Configure GenerateChangelogTask

---
+++ src/main/groovy/jarias/gradle/tasks/GenerateChangelogTask.groovy
@@ -0,0 +1,10 @@
+package jarias.gradle.tasks
+
+import org.gradle.api.DefaultTask
+
+/**
+ * @author jarias
+ * @since 4/5/13 7:07 PM 
+ */
+class GenerateChangelogTask extends DefaultTask {
+}
---
+++ src/test/groovy/jarias/gradle/tasks/GenerateChangelogTaskTest.groovy
@@ -0,0 +1,8 @@
+package jarias.gradle.tasks
+
+/**
+ * @author jarias
+ * @since 4/5/13 7:18 PM 
+ */
+class GenerateChangelogTaskTest {
+}
